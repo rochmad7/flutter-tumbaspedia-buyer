6190eb00b2ba66f5b8fb3657ad988ee20140ce20
Rochmad <rochmad.wa@gmail.com>
2022-09-08 07:57:34 +0700

    fix: dev build

--- lib/shared/constant.dart
+++ lib/shared/constant.dart
@@ -24,8 +24,9 @@ String capitalize(String string) {
 String tokenAPI =
     "aqnXBEi7KgPCMOP2qiOslLEd6u8Q2jQVFaYnlYQdtzLEVLtu0fRqTWZiPB1g";
 
-String baseURLAPI = 'http://10.0.2.2:3000/api';
+// String baseURLAPI = 'http://10.0.2.2:3000/api';
 // String baseURLAPI = 'http://192.168.1.166:3000/api';
+String baseURLAPI = 'https://dev.tumbaspedia.my.id/api';
 
 Color mainColor = "032339".toColor();
 Color greyColor = "8D92A3".toColor();
