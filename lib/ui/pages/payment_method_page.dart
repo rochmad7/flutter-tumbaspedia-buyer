ecf1e74ca95aba7d59b59fcdd3b4222d90aeb62e
Rochmad <rochmad.wa@gmail.com>
2023-05-13 20:08:08 +0700

    feat: improve info product sold and add back button on success order

--- lib/ui/pages/payment_method_page.dart
+++ lib/ui/pages/payment_method_page.dart
@@ -7,34 +7,58 @@ class PaymentMethodPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // String type = (transaction.product.category.id == 3) ? "jasa" : "barang";
-    return Scaffold(
+    return SafeArea(
+        child: Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
         backgroundColor: Colors.white,
-        body: IllustrationPage(
-          title: "Berhasil!",
-          subtitle:
-              "Selesaikan pesananmu\ndengan menghubungi\npenjual untuk\nproses pembayaran",
-          picturePath: payment,
-          buttonTap1: () async => await launch(
-            "https://wa.me/62" +
-                transaction.shop.user.phoneNumber.allAfter("0") +
-                "?text=[Pesan dari aplikasi Tumbaspedia] Halo Saya " +
-                (context.read<UserCubit>().state as UserLoaded).user.name +
-                " ingin memesan " +
-                transaction.product.name +
-                " di toko " +
-                transaction.shop.name +
-                " dengan jumlah " +
-                transaction.quantity.toString() +
-                " dan total harga " +
-                getFormatRupiah(
-                    transaction.quantity * transaction.product.price, true),
+        iconTheme: IconThemeData(color: Colors.black),
+        elevation: 0,
+        leading: GestureDetector(
+          onTap: () {
+            Get.back();
+          },
+          child: Padding(
+            padding: EdgeInsets.only(left: 16, top: 16),
+            child: Container(
+              padding: EdgeInsets.all(3),
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: mainAccentColor,
+              ),
+              child: Icon(
+                MdiIcons.arrowLeft,
+                color: Colors.white,
+                size: 30,
+              ),
+            ),
           ),
-          buttonTitle1: 'Hubungi Penjual',
-          // buttonTap1: () {
-          //   Get.to(() =>SuccessOrderPage(transaction: transaction));
-          // },
-          // buttonTitle1: 'Lanjutkan',
-        ));
+        ),
+      ),
+      body: IllustrationPage(
+        title: "Berhasil!",
+        subtitle:
+            "Selesaikan pesananmu\ndengan menghubungi\npenjual untuk\nproses pembayaran",
+        picturePath: payment,
+        buttonTap1: () async => await launch(
+          "https://wa.me/62" +
+              transaction.shop.user.phoneNumber.allAfter("0") +
+              "?text=[Pesan dari aplikasi Tumbaspedia] Halo Saya " +
+              (context.read<UserCubit>().state as UserLoaded).user.name +
+              " ingin memesan " +
+              transaction.product.name +
+              " di toko " +
+              transaction.shop.name +
+              " dengan jumlah " +
+              transaction.quantity.toString() +
+              " dan total harga " +
+              getFormatRupiah(
+                  transaction.quantity * transaction.product.price, true),
+        ),
+        buttonTitle1: 'Hubungi Penjual',
+      ),
+    ));
   }
 }
--- lib/ui/widgets/product_card.dart
+++ lib/ui/widgets/product_card.dart
@@ -15,109 +15,138 @@ class ProductCard extends StatelessWidget {
     return GestureDetector(
       onTap: press,
       child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(15),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.3),
-              spreadRadius: 1,
-              blurRadius: 5,
-              offset: Offset(0, 2), // changes position of shadow
-            ),
-          ],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              flex: 5,
-              child: ClipRRect(
-                borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
-                child: CachedNetworkImage(
-                  imageUrl: product.images,
-                  fit: BoxFit.cover,
-                  placeholder: (context, url) => CardShimmer(),
-                  errorWidget: (context, url, error) => Icon(Icons.error),
-                  repeat: ImageRepeat.repeat,
-                ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                spreadRadius: 1,
+                blurRadius: 5,
+                offset: Offset(0, 2), // changes position of shadow
               ),
-            ),
-            SizedBox(height: 10),
-            Expanded(
-              flex: 2,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      product.name,
-                      style: GoogleFonts.roboto().copyWith(
-                        color: Colors.black,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                      ),
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    SizedBox(height: 5),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Icon(
-                          MdiIcons.store,
-                          size: 20,
-                          color: Colors.grey,
-                        ),
-                        const SizedBox(width: 4),
-                        Container(
-                          width: 110,
-                          child: Text(
-                            product.shop.name,
-                            style: greyFontStyle,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
+            ],
+          ),
+          child: Stack(children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        flex: 5,
+                        child: ClipRRect(
+                          borderRadius:
+                              BorderRadius.vertical(top: Radius.circular(15)),
+                          child: CachedNetworkImage(
+                            imageUrl: product.images,
+                            fit: BoxFit.cover,
+                            placeholder: (context, url) => CardShimmer(),
+                            errorWidget: (context, url, error) =>
+                                Icon(Icons.error),
+                            repeat: ImageRepeat.repeat,
                           ),
                         ),
-                      ],
-                    ),
-                    SizedBox(height: 5),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 10),
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Text(
-                          getFormatRupiah(product.price, false),
+                          product.name,
                           style: GoogleFonts.roboto().copyWith(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600,
-                            color: mainColor,
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
                           ),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
                         ),
-                        product.sold > 0
-                            ? Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 3),
-                          decoration: BoxDecoration(
-                            color: Colors.orange,
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          child: Text(
-                            "Terjual " + formatNumber(product.sold),
-                            style: whiteFontStyle.copyWith(fontSize: 12),
-                          ),
-                        )
-                            : SizedBox(),
+                        SizedBox(height: 5),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Icon(
+                              MdiIcons.store,
+                              size: 20,
+                              color: Colors.grey,
+                            ),
+                            const SizedBox(width: 4),
+                            Container(
+                              width: 110,
+                              child: Text(
+                                product.shop.name,
+                                style: greyFontStyle,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 5),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              getFormatRupiah(product.price, false),
+                              style: GoogleFonts.roboto().copyWith(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                                color: mainColor,
+                              ),
+                            ),
+                            // product.sold > 0
+                            //     ? Container(
+                            //   padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 3),
+                            //   decoration: BoxDecoration(
+                            //     color: Colors.orange,
+                            //     borderRadius: BorderRadius.circular(15),
+                            //   ),
+                            //   child: Text(
+                            //     "Terjual " + formatNumber(product.sold),
+                            //     style: whiteFontStyle.copyWith(fontSize: 12),
+                            //   ),
+                            // )
+                            //     : SizedBox(),
+                          ],
+                        ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
-      ),
+            Positioned(
+              top: 10,
+              left: 10,
+              child: product.sold > 0
+                  ? Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 4),
+                      decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Text(
+                        "Terjual " + formatNumber(product.sold),
+                        style: whiteFontStyle.copyWith(fontSize: 13),
+                      ),
+                    )
+                  : SizedBox(),
+            ),
+          ])),
     );
   }
 }
